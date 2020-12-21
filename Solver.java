f17781cf43de13bca533a3ca4bc04e6258225a74
sudoerena <sudoerena@users.noreply.github.com>
2020-12-20 20:31:08 -0500

    To fix: trial & error bug

--- Solver.java
+++ Solver.java
@@ -14,25 +14,30 @@ public class Main {
 
 public class Solver {
     Puzzle puzz;
+    //int invalid[][][];
     int invalid[][][] = new int[9][9][10];
+
     Solver () {
+        //invalid = new int[9][9][10];
+
         Scanner sc = new Scanner(System.in);
         int in;
 
         while (true) {
-            System.out.print("Enter 0 for file input, or 1 for manual input: ");
+            System.out.print("Enter 1 for file input, or 2 for manual input: ");
             in = sc.nextInt();
-            if (in == 0 || in == 1) {
+            sc.nextLine();
+            if (in == 1 || in == 2) {
                 break;
             }
             System.out.println("Please enter a valid choice.");
         }
 
-        if (in == 0) {
+        if (in == 1) {
             System.out.print("Please enter puzzle file name: ");
             puzz = new Puzzle(sc.nextLine());
         }
-        else if (in == 1) {
+        else if (in == 2) {
             System.out.print("Puzzle size: ");
             puzz = new Puzzle(sc.nextInt());
 
@@ -45,157 +50,176 @@ public class Solver {
         }
     }
 
-    Solver (String name) {
-        puzz = new Puzzle(name);
-    }
-
     Solver (Puzzle puzz) {
         this.puzz = puzz;
     }
 
-    boolean solve () {
-        // SOLVE puzzle and return value
-        /*
-         * 1. search for blank
-         * 2. check lines
-         * 3. check square
-         * 4. if only one valid, EDIT
-         *      else, return to 1.
-         *  If repeats ~80 times with no change,
-         *      NEW puzzle
-         *      put down random valid number and solve for that
-         *      need invalid checker
-         *      --> return all blank if invalid?
-         *  If invalid, set that num to invalid
-         */
-        int x = 0, y = 0, inv[], failed = 0;
+    void print () {
+        puzz.print();
+    }
 
+    // return false if failed because invalid
+    boolean solve () {
+        /*
+         * v2:
+         * IF BLANK:
+         * 1. tryFill()
+         *
+         * ELSE:
+         * 1. add self to every row, col, box
+         */
+
+        updateInvalid();
+
+        int x = 0, y = 0, failed = 0;
         while (true) {
             // check full (solved)
             if (puzz.isFull()) {
+                if (puzz.isSolved()) {
+                    System.out.println("Solved it!");
+                }
                 break;
             }
 
-            // get next blank space
-            int temp = x;
-            x = nextX(x, y);
-            y = nextY(temp, y);
-            inv = invalid[x][y];
+            // move to next space
+            if (x == 8) {
+                x = 0;
 
-            // check horizontal
-            for (int j = 0; j < 9; j++) {
-                inv[puzz.getGrid(x, j)] = 1;
-            }
-
-            // check vertical
-            for (int i = 0; i < 9; i++) {
-                inv[puzz.getGrid(i, y)] = 1;
-            }
-
-            // check square
-            int x0 = 3*(x/3), y0 = 3*(y/3);
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    inv[puzz.getGrid(i + x0, j + y0)] = 1;
+                if (y == 8) {
+                    y = 0;
+                }
+                else {
+                    y++;
                 }
             }
-
-            // try to fill
-            int success = tryFill(x, y, inv);
-            if (success == -1) {
-                failed++;
-            }
-            else if (success == -2) {
-                puzz.clear();
-                break;
+            else {
+                x++;
             }
 
-            // unchanged for many steps; trial and error
-            if (failed == 80) {
-                failed = 0;
+            // if blank cell
+            if (puzz.getGrid(x, y) == 0) {
+                // try to fill
+                int success = tryFill(x, y);
+                if (success == -1) {
+                    failed++;
 
-                int tryNum = 0;
+                    // unchanged for many steps; trial and error
+                    if (failed >= 1000) {
+                        failed = 0;
+                        updateInvalid();
 
-                for (int i = 1; i < 10; i++) {
-                    if (inv[i] == 0) {
-                        tryNum = i;
-                        break;
+                        for (int i = 1; i < 10; i++) {
+                            if (invalid[x][y][i] == 0) {
+                                // new puzzle with random valid #
+                                Puzzle tryPuzz = new Puzzle(puzz);
+                                tryPuzz.putGrid(x, y, i);
+                                Solver trySolve = new Solver(tryPuzz);
+
+                                // if was invalid
+                                if (!trySolve.solve()) {
+                                    invalid[x][y][i] = 1;
+                                }
+                                else {
+                                    //puzz.putGrid(x, y, i);
+                                    puzz = tryPuzz;
+                                    return true;
+                                }
+
+                                break;
+                            }
+                        }
                     }
                 }
-
-                // new puzzle with random valid #
-                Puzzle tryPuzz = puzz;
-                tryPuzz.putGrid(x, y, tryNum);
-                Solver trySolve = new Solver(tryPuzz);
-
-                // if was invalid
-                if (trySolve.solve()) {
-                    inv[tryNum] = 1;
+                else if (success == -2) {
+                    System.out.println("Invalid result.");
+                    return false;
                 }
                 else {
-                    puzz.putGrid(x, y, tryNum);
+                    failed = 0;
+                    updateInvalid(x, y);
                 }
             }
-
-            // update invalid
-            invalid[x][y] = inv;
+            // if filled cell
+            else {
+                updateInvalid(x, y);
+            }
         }
 
-        return (puzz.getGrid(0, 0) == 0);
+        return true;
     }
 
-    int nextX (int x, int y) {
-        do {
-            if (x == 8) {
-                x = 0;
-                if (y == 8) {
-                    y = 0;
+    void updateInvalid () {
+        for (int x = 0; x < 9; x++) {
+            for (int y = 0; y < 9; y++) {
+                // mark horizontals and verticals
+                for (int i = 0; i < 9; i++) {
+                    invalid[x][i][puzz.getGrid(x, y)] = 1;
+                    invalid[i][y][puzz.getGrid(x, y)] = 1;
                 }
-                else {
-                    y++;
-                }
-            }
-            else {
-                x++;
-            }
-        } while (puzz.getGrid(x, y) != 0);
 
-        return x;
+                // mark box
+                int x0 = x / 3, y0 = y / 3;
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+                        invalid[i+3*x0][j+3*y0][puzz.getGrid(x, y)] = 1;
+                    }
+                }
+            }
+        }
     }
 
-    int nextY (int x, int y) {
-        do {
-            if (x == 8) {
-                x = 0;
-                if (y == 8) {
-                    y = 0;
-                }
-                else {
-                    y++;
-                }
-            }
-            else {
-                x++;
-            }
-        } while (puzz.getGrid(x, y) != 0);
+    void updateInvalid (int x, int y) {
+        // mark horizontals and verticals
+        for (int i = 0; i < 9; i++) {
+            invalid[x][i][puzz.getGrid(x, y)] = 1;
+            invalid[i][y][puzz.getGrid(x, y)] = 1;
+        }
 
-        return y;
+        // mark box
+        int x0 = x / 3, y0 = y / 3;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                invalid[i+3*x0][j+3*y0][puzz.getGrid(x, y)] = 1;
+            }
+        }
     }
 
-    int tryFill (int x, int y, int in[]) {
-        int count = 0, val = 0;
+    void printInvalid () {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+
+                if (puzz.getGrid(i,j) == 0) {
+                    System.out.printf("(%d, %d): ", i, j);
+                    for (int k = 1; k < 10; k++) {
+                        if (invalid[i][j][k] == 1) {
+                            System.out.print(k);
+                        }
+                        else {
+                            System.out.print(0);
+                        }
+                    }
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+
+    int tryFill (int x, int y) {
+        int count = 0, val = -1;
 
         // count valid numbers
         for (int i = 1; i < 10; i++) {
-            if (in[i] == 0) {
+            if (invalid[x][y][i] == 0) {
                 val = i;
                 count++;
             }
         }
 
-        // more than one valid choice
-        if (count > 1) {
-            return -1;
+        // works
+        if (count == 1) {
+            puzz.putGrid(x, y, val);
+            return 0;
         }
 
         // no valid choices
@@ -203,17 +227,8 @@ public class Solver {
             return -2;
         }
 
-        puzz.putGrid(x, y, val);
-        return 0;
-    }
-
-    void print () {
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                System.out.print(puzz.grid[i][j] + " ");
-            }
-            System.out.println();
-        }
+        // more than one valid choice
+        return -1;
     }
 }
 
@@ -226,6 +241,10 @@ public class Puzzle{
 
     Puzzle (int num) {
         grid = new int[num][num];
+    }
+
+    Puzzle (Puzzle puzz) {
+        this.grid = puzz.grid;
     }
 
     Puzzle (String name) {
@@ -260,27 +279,85 @@ public class Puzzle{
         }
     }
 
-    void putGrid (int x, int y, int n) {
-        grid[x][y] = n;
-        return;
-    }
-
-    int getGrid (int x, int y) {
-        return this.grid[x][y];
-    }
-
-    boolean isFull () {
-        boolean full = true;
-
+    boolean isBlank () {
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
-                if (grid[i][j] == 0) {
-                    full = false;
-                    break;
+                if (grid[i][j] != 0) {
+                    return false;
                 }
             }
         }
 
-        return full;
+        return true;
+    }
+
+    void print () {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(grid[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    void putGrid (int x, int y, int n) {
+        grid[x][y] = n;
+    }
+
+    int getGrid (int x, int y) {
+        return grid[x][y];
+    }
+
+    boolean isFull () {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (grid[i][j] == 0) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    boolean isSolved () {
+        // check horizontals and verticals
+        for (int i = 0; i < 9; i++) {
+            int xs[] = new int[9], ys[] = new int[9];
+            for (int j = 0; j < 9; j++) {
+                if (xs[grid[i][j] - 1] == 1) {
+                    return false;
+                }
+                else {
+                    xs[grid[i][j] - 1] = 1;
+                }
+                if (ys[grid[j][i] - 1] == 1) {
+                    return false;
+                }
+                else {
+                    ys[grid[j][i] - 1] = 1;
+                }
+            }
+        }
+
+        // check boxes
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                int nums[] = new int[9];
+
+                for (int x = 0; x < 3; x++) {
+                    for (int y = 0; y < 3; y++) {
+                        if (nums[grid[3*i+x][3*j+y] - 1] == 1) {
+                            return false;
+                        }
+                        else {
+                            nums[grid[3*i+x][3*j+y] - 1] = 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        return true;
     }
 }
